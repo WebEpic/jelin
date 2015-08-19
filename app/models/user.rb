6af46a01105ae1c851593282ff287f5c760e574f
WebEpic <randy@randyjmitchell.com>
2015-08-19 00:04:20 -0400

    add associations for location and checkin models

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :checkins
+  has_many :locations, :through => :checkins
 end
